baa4560b51f09b86a1b09d547ce54848105fd1d1
alekhya24 <alekhya.karicharla9@gmail.com>
2019-03-08 01:09:04 -0500

    Commit 5

--- answers/answer.py
+++ answers/answer.py
@@ -98,10 +98,10 @@ def data_frame(filename, n):
     spark = init_spark()
     lines = spark.read.text(filename)
     '''parts = lines.map(lambda row: row.split(","))'''
-    test_rate = lines.toDF().take(n)
+    test_rate = lines.take(n)
     final = toCSVLine(test_rate)
     print(final)
-    ratings =lines.toDF()
+    ratings =lines
     final_op = toCSVLine(ratings)
     return final_op
     '''return "not implemented"'''
