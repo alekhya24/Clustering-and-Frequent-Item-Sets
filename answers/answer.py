61c2eb198c7c1f34a73121f6378b66e65881afbb
alekhya24 <alekhya.karicharla9@gmail.com>
2019-03-13 14:35:59 -0400

    Commit

--- answers/answer.py
+++ answers/answer.py
@@ -244,8 +244,7 @@ def data_preparation(filename, plant, state):
     rdd = sc.parallelize(tuple_list[1:])
     data_f = spark.createDataFrame(rdd)
     final_op = data_f.select(data_f._2).where(data_f._1 == state).collect()
-    print(final_op[0][0])
-    if  plant in final_op[0][0]:
+    if  plant in final_op.asDict().keys():
         return True
     else:
         return False
